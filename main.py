1fdef24477c18968e122fc20a0f6bc0687cf253d
Scorwx <102318206+Scorwx@users.noreply.github.com>
2024-04-03 20:28:52 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+import requests
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api', methods=['GET'])
+def hello():
+    args = request.args
+    term = args.get("term")
+    response = requests.get(f"https://www.enuygun.com/ucak-bileti/trip-autocomplete/?term={term}")
+    data = response.json()
+
+    airports = []
+    for airport_info in data:
+        airport = {
+            "airport": airport_info['airport'],
+            "city": airport_info['city_name'],
+            "country": airport_info['country_name'],
+            "id": airport_info['id']
+        }
+        airports.append(airport)
+    
+    return jsonify(airports)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
